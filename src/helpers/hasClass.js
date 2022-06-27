e231a273385ba9ebe08e3504e1bddcc5c7de67b5
Mor <mortogo321@gmail.com>
2022-06-27 23:26:47 +0700

    Cannot read property 'classList' of undefined

--- src/helpers/hasClass.js
+++ src/helpers/hasClass.js
@@ -1,7 +1,11 @@
-import { classListSupport } from './classListSupport.js';
+import { classListSupport } from "./classListSupport.js";
 
-var hasClass = classListSupport ?
-    function (el, str) { return el.classList.contains(str); } :
-    function (el, str) { return el.className.indexOf(str) >= 0; };
+var hasClass = classListSupport
+  ? function (el, str) {
+      return el && el.classList.contains(str);
+    }
+  : function (el, str) {
+      return el && el.className.indexOf(str) >= 0;
+    };
 
 export { classListSupport, hasClass };
--- src/helpers/removeClass.js
+++ src/helpers/removeClass.js
@@ -1,10 +1,14 @@
-import { classListSupport, hasClass } from './hasClass.js';
-var removeClass = classListSupport ?
-    function (el, str) {
-      if (hasClass(el,  str)) { el.classList.remove(str); }
-    } :
-    function (el, str) {
-      if (hasClass(el, str)) { el.className = el.className.replace(str, ''); }
+import { classListSupport, hasClass } from "./hasClass.js";
+var removeClass = classListSupport
+  ? function (el, str) {
+      if (el && hasClass(el, str)) {
+        el.classList.remove(str);
+      }
+    }
+  : function (el, str) {
+      if (el && hasClass(el, str)) {
+        el.className = el.className.replace(str, "");
+      }
     };
 
 export { removeClass };
